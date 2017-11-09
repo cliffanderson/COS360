9bc623647f5b63127d02033ad012c490bacfc5da
Cliff Anderson <canderson@onecloudlabs.com>
2017-11-09 12:05:42 -0500

    Add correct indents and notes

--- hm4/setTranslator.java
+++ hm4/setTranslator.java
@@ -23,48 +23,63 @@ Modified 10/23/17
 */
 public class setTranslator{
 
-   private static setScanner sc;
+    private static setScanner sc;
 
-   // this should be created if the parse gets as far as the identifier that
-   // follows "program"
-   private static PrintWriter dest;
+    // this should be created if the parse gets as far as the identifier that
+    // follows "program"
+    private static PrintWriter dest;
 
-   private static void program(){
-   // attempts the translation of the source file 
-   // into a Java source file 
-   // YOU HAVE TO CODE THIS
-
-   }
+    private static void program(){
+        // attempts the translation of the source file
+        // into a Java source file
+        // YOU HAVE TO CODE THIS
 
 
-// you should not modify the main method
-public static void main(String[] args) throws Exception{
 
-   if (args.length == 0)
-      sc = new setScannerSolution(new Scanner(System.in));
-   else
-      sc = new setScannerSolution(new Scanner(new File(args[0])));
+        //print public class <class> {
+        //print     public static void main(String[] args) {
 
-   Token currTok = sc.lookahead();
 
-   try{
-      if (Token.programSet.contains(currTok.getTokenType())){
-         program();
-         // add a comment  indicating a successful parse
-         if (dest != null){
-            dest.println("\n// Successful parse.");
-            dest.close();
-         }
-      }
-      else
-         // you should write a method to take the set of tokens that would
-         // be okay and the line number to create and throw the exception with
-         // the right error message for the other error messages
-         throw new Exception("[line " + currTok.getLineNum() + "]: \"program\" expected.");
-   }
-   catch (Exception e){
-      // if the translation throws an exception, it is written to standard error
-      System.err.println(e.getMessage());
-   }
-}
+        
+        //.....
+
+
+        //print     }
+        //print }
+        //print successful parse
+
+
+    }
+
+
+    // you should not modify the main method
+    public static void main(String[] args) throws Exception{
+
+        if (args.length == 0)
+            sc = new setScannerSolution(new Scanner(System.in));
+        else
+            sc = new setScannerSolution(new Scanner(new File(args[0])));
+
+        Token currTok = sc.lookahead();
+
+        try{
+            if (Token.programSet.contains(currTok.getTokenType())){
+                program();
+                // add a comment  indicating a successful parse
+                if (dest != null){
+                    dest.println("\n// Successful parse.");
+                    dest.close();
+                }
+            }
+            else
+                // you should write a method to take the set of tokens that would
+                // be okay and the line number to create and throw the exception with
+                // the right error message for the other error messages
+                throw new Exception("[line " + currTok.getLineNum() + "]: \"program\" expected.");
+        }
+        catch (Exception e){
+            // if the translation throws an exception, it is written to standard error
+            System.err.println(e.getMessage());
+        }
+    }
 }
