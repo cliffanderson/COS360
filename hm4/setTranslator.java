54f1d6b208db071586a7e97c439ab5169f0a89ac
Natasha <Natasha.Wrobel@maine.edu>
2017-11-30 16:08:12 -0500

    Assignment not working perfectly but it's a start. I'll try to get back to it tonight.

--- hm4/setTranslator.java
+++ hm4/setTranslator.java
@@ -28,7 +28,7 @@ public class setTranslator{
     private static Map<String, CofinFin> setVariables = new HashMap<String, CofinFin>();
 
 
-   public static void program() throws Exception {
+    public static void program() throws Exception {
 
         System.out.println("program()");
 
@@ -50,6 +50,12 @@ public class setTranslator{
         doVar();
 
 
+        if(sc.lookahead().getTokenType() == Token.BEGIN)
+            doBegin();
+        else
+            throw new Exception("Missing Begin token");
+
+
 //        else {
 //            sc.consume();
 //            if(sc.lookahead().getTokenType() != Token.ID) {
@@ -66,11 +72,11 @@ public class setTranslator{
         while(! tokenType.equals("eof")) {
             tokenType = Token.TOKEN_LABELS[sc.lookahead().getTokenType()];
             String tokenStringValue = sc.lookahead().getTokenString();
-   
-                    System.out.printf("Token type: %s          Token string value: %s%n",
-                            tokenType,
-                            tokenStringValue);
-                sc.consume();                   
+
+            System.out.printf("Token type: %s          Token string value: %s%n",
+                    tokenType,
+                    tokenStringValue);
+            sc.consume();
         }
     }
 
@@ -138,8 +144,8 @@ public class setTranslator{
         List<String> natVariables = new ArrayList<String>();
 
         // while not semicolon
-            // consume ID and create nat
-            // if not comma, break;
+        // consume ID and create nat
+        // if not comma, break;
 
         while(sc.lookahead().getTokenType() != Token.SEMICOLON) {
             if(sc.lookahead().getTokenType() != Token.ID) {
@@ -219,6 +225,193 @@ public class setTranslator{
             System.out.println("    private static Cofin " + varName + " = null;");
             setVariables.put(varName, null);
         }
+    }
+
+
+    private static void doBegin() throws Exception {
+        sc.consume();
+
+        TreeSet<Integer> calculationSet = new TreeSet<Integer>();
+        calculationSet.add(Token.COMPLEMENT);
+        calculationSet.add(Token.IF);
+        calculationSet.add(Token.INTERSECTION);
+        calculationSet.add(Token.SETDIFFERENCE);
+        calculationSet.add(Token.UNION);
+
+        int lookAheadType = sc.lookahead().getTokenType();
+        String varName = null;
+        if (lookAheadType == Token.ID) {
+            varName = sc.lookahead().getTokenString();
+            sc.consume();
+        }
+        else {
+            int x = 0;
+            //its not doing assignments or there's an error. Still needs to be handled
+        }
+        lookAheadType = sc.lookahead().getTokenType();
+
+        String wasAssigned = "";
+
+        while (!calculationSet.contains(lookAheadType)) {
+            if (varName == null)
+                break;
+            if (naturalVariables.containsKey(varName))
+                assignNat(varName);
+            else if (setVariables.containsKey(varName))
+                wasAssigned = assignSet(varName, calculationSet);
+            else
+                throw new Exception("Variable not declared.");
+            if (!wasAssigned.equals("$")){
+                throw new Exception("Something went wrong...");
+            }
+            lookAheadType = sc.lookahead().getTokenType();
+            if (lookAheadType == Token.ID) {
+                varName = sc.lookahead().getTokenString();
+                sc.consume();
+                lookAheadType = sc.lookahead().getTokenType();
+            }
+            else
+                break;
+
+        }
+
+    }
+
+    //Not completely done!!!
+    //so far...
+    //this takes the sc after the varName is consumed
+    //it checks for the assignment token and looks till it finds a semi colon or a "calculation" token
+    //assuming it is an assignment it does the assignment
+    //(Though for the moment it can't handle empty string. I try to fix that sometime tonight.)
+    //otherwise there are some weird returns to handle other situations
+    //if it hits a "calculation" token it returns the previous variable so we don't loose it
+    //and doesn't consume the token so if it is a calculation we should be able to proceed accordingly.
+    // it might be doing other weird stuff, haven't full tested it yet
+
+    //occasionally throwing a variable not assigned error...not sure why yet...
+
+    private static String assignSet(String varName, TreeSet<Integer> calculationSet) throws Exception {
+        int la = sc.lookahead().getTokenType();
+        String tempName;
+
+        if(la == Token.ASSIGN) {
+            sc.consume();
+            la = sc.lookahead().getTokenType();
+            if (calculationSet.contains(la))
+                return null;
+            if (la == Token.ID) {
+                tempName = sc.lookahead().getTokenString();
+                if (setVariables.containsKey(tempName)) {
+                    if (setVariables.get(tempName) == null)
+                        throw new Exception("Variable " + varName + " not yet assigned.");
+                    sc.consume();
+                    la = sc.lookahead().getTokenType();
+                    if (calculationSet.contains(la))
+                        return tempName;
+                    else if (la == Token.SEMICOLON) {
+                        System.out.println("        " + varName + " = " + tempName + ";");
+                        sc.consume();
+                        return "$";
+                    }
+                    else
+                        throw new Exception("Semicolon expected");
+
+                }
+                else if (naturalVariables.containsKey(tempName)) {
+                    if (naturalVariables.get(tempName) == null)
+                        throw new Exception("Variable " + varName + " not yet assigned.");
+                    sc.consume();
+                    la = sc.lookahead().getTokenType();
+                    if (la == Token.SEMICOLON) {
+                        System.out.println("        " + varName + " = new Cofin(false, " + tempName + ");");
+                        sc.consume();
+                        return "$";
+                    }
+                    else
+                        throw new Exception("Semicolon Expected");
+
+                }
+                else
+                    throw new Exception("Variable " + tempName + " not declared.");
+            }
+            String comp = "false";
+            ArrayList<String> constructorValues= new ArrayList<String>();
+            if (la == Token.CMP) {
+                comp = "true";
+                sc.consume();
+                la = sc.lookahead().getTokenType();
+            }
+            if (la == Token.LEFTBRACE) {
+                sc.consume();
+                la = sc.lookahead().getTokenType();
+                String curr;
+                while (la == Token.NATCONST) {
+                    curr = sc.lookahead().getTokenString();
+                    constructorValues.add(curr);
+                    sc.consume();
+                    la = sc.lookahead().getTokenType();
+                    if (la != Token.COMMA) {
+                        break;
+                    }
+                    sc.consume();
+                    la = sc.lookahead().getTokenType();
+                }
+                if (la != Token.RIGHTBRACE)
+                    throw new Exception("Right brace expected.");
+                sc.consume();
+                la = sc.lookahead().getTokenType();
+                if (la != Token.SEMICOLON)
+                    throw new Exception("Semicolon expected.");
+                sc.consume();
+                System.out.print("        int[] $" + varName + " = {");
+                for (int i = 0; i < constructorValues.size()-1; i++) {
+                    System.out.print(constructorValues.get(i) + ", ");
+                }
+                System.out.println(constructorValues.get(constructorValues.size()-1) + "};");
+                System.out.println("        " + varName + " = new Confin(" + comp + ", $" + varName + ");");
+                return "$";
+            }
+
+        }
+
+        return "Something fishy...";
+
+    }
+
+    //takes the sc if its encountered a nat variable name but before the name has been consumed.
+    //returns the sc after consuming the semicolon after the assignment.
+    //mostly working as far as I know...
+
+    private static void assignNat(String varName) throws Exception {
+        sc.consume();
+        String natValue;
+        int la = sc.lookahead().getTokenType();
+        if (la == Token.ASSIGN) {
+            sc.consume();
+            la = sc.lookahead().getTokenType();
+            if (la == Token.NATCONST)
+                natValue =sc.lookahead().getTokenString();
+            else if (la == Token.ID) {
+                natValue = sc.lookahead().getTokenString();
+                if (!naturalVariables.containsKey(natValue))
+                    throw new Exception("Variable " + natValue + " not declared.");
+                else if (naturalVariables.get(natValue) == null)
+                    throw new Exception("Variable " + varName + " not yet assigned.");
+            }
+            else
+                throw new Exception("Constant value expected.");
+            System.out.println("        " + varName + " = " + natValue + ";");
+            naturalVariables.put(varName, Integer.getInteger(natValue));
+            sc.consume();
+            la = sc.lookahead().getTokenType();
+            if (la != Token.SEMICOLON)
+                throw new Exception("Semicolon expected.");
+            sc.consume();
+            return;
+        }
+        else
+            throw new Exception ("Assignment expected");
+
     }
 
 
